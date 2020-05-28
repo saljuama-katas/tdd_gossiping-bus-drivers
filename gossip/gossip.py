55e3535e4f443bc83721f28e221020f3e0b66ddc
Salvador Juan Martinez <saljuama@gmail.com>
2020-05-28 23:55:11 +0200

    generalise for 2 routes in the same stop

--- gossip/gossip.py
+++ gossip/gossip.py
@@ -2,9 +2,8 @@ def gossip(routes):
     if len(routes) < 2:
         return None
 
-    if routes[0][0] == routes[1][0]:
-        return 0
-    elif routes[0][1] == routes[1][1]:
-        return 1
-    else:
-        return None
+    for x in range(3):
+        if routes[0][x] == routes[1][x]:
+            return x
+
+    return None
--- gossip/test/test_gossip.py
+++ gossip/test/test_gossip.py
@@ -8,17 +8,12 @@ def test_gossips_never_spread_when_there_is_only_one_route():
     assert gossip(bus_routes) is None
 
 
-def test_all_gossip_is_spread_when_all_routes_start_in_the_same_stop():
-    bus_routes = [
-        [1, 2, 3, 4],
-        [1, 5, 7, 8]
-    ]
+def test_all_gossip_is_spread_in_n_minutes_if_all_routes_share_the_n_plus_one_stop():
+    bus_routes = [[1, 2, 3], [1, 4, 5]]
     assert gossip(bus_routes) is 0
 
-
-def test_all_gossip_is_spread_in_one_minute_if_all_routes_share_the_second_stop():
-    bus_routes = [
-        [1, 5, 3, 4],
-        [2, 5, 7, 8]
-    ]
+    bus_routes = [[1, 2, 3], [4, 2, 5]]
     assert gossip(bus_routes) is 1
+
+    bus_routes = [[1, 2, 3], [4, 5, 3]]
+    assert gossip(bus_routes) is 2
