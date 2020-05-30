fab4d394ee01d57d0c6293054d22c6179f96ae90
Salvador Juan Martinez <saljuama@gmail.com>
2020-05-30 17:39:33 +0200

    refactor gossip calculation into its own class
    - pre-refactor for next use cases with more than 2 drivers

--- gossip/gossip.py
+++ gossip/gossip.py
@@ -1,11 +1,13 @@
-def gossip(routes):
-    drivers = [BusDriver(route) for route in routes]
-    if len(drivers) > 1:
-        for x in range(480):
-            if drivers[0].stop_at_minute(x) == drivers[1].stop_at_minute(x):
-                return x
+class GossipCalculator:
+    def __init__(self, bus_routes):
+        self.drivers = [BusDriver(route) for route in bus_routes]
 
-    return None
+    def gossip_spread_time(self):
+        if len(self.drivers) > 1:
+            for x in range(480):
+                if self.drivers[0].stop_at_minute(x) == self.drivers[1].stop_at_minute(x):
+                    return x
+        return None
 
 
 class BusDriver:
--- gossip/test/test_gossip.py
+++ gossip/test/test_gossip.py
@@ -1,35 +1,43 @@
-from ..gossip import gossip
+from ..gossip import GossipCalculator
 
 
 def test_gossips_never_spread_when_there_is_only_one_route():
     bus_routes = [[1, 2, 3, 4]]
-    assert gossip(bus_routes) is None
+    calculator = GossipCalculator(bus_routes)
+    assert calculator.gossip_spread_time() is None
 
 
 def test_gossips_never_spread_with_multiple_routes_that_never_share_a_stop():
     bus_routes = [[1, 2, 3], [4, 5, 6]]
-    assert gossip(bus_routes) is None
+    calculator = GossipCalculator(bus_routes)
+    assert calculator.gossip_spread_time() is None
 
 
 def test_all_gossip_is_spread_in_n_minutes_if_all_routes_share_the_n_plus_one_stop():
     bus_routes = [[1, 2, 3], [1, 4, 5]]
-    assert gossip(bus_routes) == 0
+    calculator = GossipCalculator(bus_routes)
+    assert calculator.gossip_spread_time() == 0
 
     bus_routes = [[1, 2, 3], [4, 2, 5]]
-    assert gossip(bus_routes) == 1
+    calculator = GossipCalculator(bus_routes)
+    assert calculator.gossip_spread_time() == 1
 
     bus_routes = [[1, 2, 3], [4, 5, 3]]
-    assert gossip(bus_routes) == 2
+    calculator = GossipCalculator(bus_routes)
+    assert calculator.gossip_spread_time() == 2
 
 
 def test_drivers_repeat_the_route_once_they_finish():
     bus_routes = [[1, 2], [3, 4, 5, 6, 7, 8, 1]]
-    assert gossip(bus_routes) == 6
+    calculator = GossipCalculator(bus_routes)
+    assert calculator.gossip_spread_time() == 6
 
 
 def test_drivers_drive_a_maximum_of_480_stops():
     bus_routes = [[1000, 480], list(range(1, 481))]
-    assert gossip(bus_routes) == 479
+    calculator = GossipCalculator(bus_routes)
+    assert calculator.gossip_spread_time() == 479
 
     bus_routes = [[481, 1000], list(range(1, 482))]
-    assert gossip(bus_routes) is None
+    calculator = GossipCalculator(bus_routes)
+    assert calculator.gossip_spread_time() is None
